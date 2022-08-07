2b8d8d960badc077e287b769b63db7442cabe618
martijnschermers <martijnschermers2@gmail.com>
2022-08-07 17:03:41 +0200

    Cleaned code by adding functions, added progresbar

--- main.py
+++ main.py
@@ -1,68 +1,84 @@
 import imaplib
 import getpass
 from pick import pick
+from progress.bar import Bar
 
 def main(): 
+  print("Simple program to remove all emails from a selected mailbox. If you want to leave the program, you can press CTRL+C at any time.")
   providers = ["Gmail", "Outlook"]
   title = "Please select the provider to remove emails from: "
   provider, index = pick(providers, title, indicator=">")
 
-  print("Simple program to remove all emails from a selected mailbox. If you want to leave the program, you can press CTRL+C at any time.")
-  email = input("Enter your email: ")
-
-  print("The password is not visible due to security reasons. Please enter it manually. See the README.md for more information.")
-  password = getpass.getpass(prompt="Enter your app password: ")
-
-  imap = ""
   if provider == "Gmail":
     imap = "imap.gmail.com"
   else: 
     imap = "outlook.office365.com"
 
-  box = imaplib.IMAP4_SSL(imap)
-  try: 
-    box.login(email, password)
-  except:
-    print("Invalid email or password.")
-    exit()
+  def login(): 
+    email = input("Enter your email: ")
+
+    print("The app password is not visible due to security reasons. Please enter it manually. See the README.md for more information.")
+    password = getpass.getpass(prompt="Enter your app password: ")
+
+    global box
+    box = imaplib.IMAP4_SSL(imap)
+    try: 
+      box.login(email, password)
+    except:
+      print("Invalid email or password. Try again.")
+      login()
+
+  login()
 
   mailboxes = box.list()[1]
   for i in range(len(mailboxes)):
-    mailbox = mailboxes[i].decode("utf-8").split(' "/" ')[1]
-    mailboxes[i] = mailbox
+    decoded_mailbox = mailboxes[i].decode("utf-8").split(' "/" ')[1]
+    mailboxes[i] = decoded_mailbox
 
   if provider == "Gmail":
     mailboxes.pop(1) # Remove the [Gmail] mailbox
 
-  title = "Please select the mailbox to remove emails from: "
-  mailbox, index = pick(mailboxes, title, indicator=">")
+  def select_mailbox():
+    title = "Please select the mailbox to remove emails from: "
+    global mailbox
+    mailbox, index = pick(mailboxes, title, indicator=">")
 
-  box.select(mailbox)
-  typ, data = box.search(None, "ALL")
-  emails = data[0].split()
+    box.select(mailbox)
+    typ, data = box.search(None, "ALL")
+    global emails
+    emails = data[0].split()
 
-  amount = len(emails)
-  print(f"Amount of emails in {mailbox}: ", amount)
+    global amount
+    amount = len(emails)
+    if amount == 0:
+      print("There are no emails in this mailbox. Select another one.")
+      select_mailbox()
+    
+    print(f"Amount of emails in {mailbox}: ", amount)
+
+  select_mailbox()
 
   title = "Remove the emails instantly or move the emails to trash?"
   options = ["Remove Instantly", "Move to Trash"]
   remove_option, index = pick(options, title, indicator=">")
 
   def remove(): 
+    bar = Bar('Removing', max=amount)
+
     if remove_option == "Remove Instantly":
       for email in emails:
+        bar.next()
         box.store(email, "+FLAGS", "\\Deleted")
       box.expunge() # Removes the emails that are marked as Deleted
-      print("Emails removed.")
     else: 
       for email in emails:
-        box.store(email, "+X-GM-LABELS", "\\Trash")  
-      print("Emails moved to trash.")
+        bar.next()
+        box.store(email, "+X-GM-LABELS", "\\Trash")
 
+    bar.finish()
     box.close()
     box.logout()
 
-  confirm_text = ""
   if remove_option == "Remove Instantly":
     confirm_text = f"Are you sure you want to remove all emails from {mailbox}? (y/n) "
   else:
@@ -79,5 +95,5 @@ if __name__ == "__main__":
   try: 
     main()
   except(KeyboardInterrupt): 
-    print("Exiting...")
+    print("\nExiting...")
     exit()
